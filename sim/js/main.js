70573ed419b3d2ea0992b7d150513d6d8abae624
helo <132366128+hefugu@users.noreply.github.com>
2026-07-10 09:24:56 +0900

    Wire 3F sample map loader

--- sim/js/main.js
+++ sim/js/main.js
@@ -1,5 +1,7 @@
 import { initUI } from "./ui.js";
 import { initSimulation } from "./simulation/core.js";
+import { setupSample3FLoader } from "./sample3f-loader.js";
 
 initUI();
 initSimulation();
+setupSample3FLoader();
